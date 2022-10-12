a6469962706be301c2444c5fc9f3263ff80ea1c4
Alvin Kong <alvink7nz@gmail.com>
2022-10-12 14:10:08 +1300

    add projects

--- code-camp/test.py
+++ code-camp/test.py
@@ -1,7 +1,5 @@
 import winsound
-
-def make_sound():
-    for i in range(40):
-        winsound.Beep(2000, 200)
-
-make_sound()
+sec = input('input a num')
+sec = int(sec)
+if sec/60 == int:
+    winsound.Beep(2000, 1000)

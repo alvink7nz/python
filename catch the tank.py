c44f2d2e5479b0e5156953274978403bf53c62ec
Alvin Kong <alvink7nz@gmail.com>
2023-01-11 11:31:19 +1300

    add projects

---
+++ catch the tank.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+img = Image.open('python/images/tank.jpg')
+img2 = Image.open('python/images/tank2.jpg')
+window = Image.
--- super calculator.py
+++ super calculator.py
@@ -1,1 +1,4 @@
 from tkinter import *
+root = Tk()
+c = Canvas(height=)
+Button(c, text='1', height=2, width=2)

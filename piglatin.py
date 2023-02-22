1acc282d191dda11f6aad71265933a17b8e12680
alvink7nz <106409444+alvink7nz@users.noreply.github.com>
2023-02-23 12:58:10 +1300

    Create piglatin.py

---
+++ piglatin.py
@@ -0,0 +1,8 @@
+encode = input('Enter a word for pig latin')
+change_message = []
+constanants = []
+def encode_message():
+    global constanants, change_message
+    index = 0
+    while index <= len(encode):
+        

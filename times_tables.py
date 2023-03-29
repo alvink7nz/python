02b7c9c02ca0a06b44c9d341c1577617393b08e1
alvink7nz <106409444+alvink7nz@users.noreply.github.com>
2023-03-30 11:00:47 +1300

    Create times_tables.py

---
+++ times_tables.py
@@ -0,0 +1,5 @@
+import math
+learning = input('press q to quit, press l to learn and t to test.')
+if learning == 'l':
+	tables = input('what table do you want to practice?')
+print('{} x 1').format(tables)
